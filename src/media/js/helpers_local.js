7ac92e1b2d44e08fc7acf92c7af79b6588665bdd
Chuck Harmston <chuck@chuckharmston.com>
2014-11-03 13:18:37 -0700

    Add operator shelf listing page (bug 1074428)

--- src/media/js/helpers_local.js
+++ src/media/js/helpers_local.js
@@ -1,8 +1,30 @@
-define('helpers_local', ['nunjucks'], function(nunjucks) {
+define('helpers_local', ['nunjucks', 'operators', 'underscore'],
+    function(nunjucks, operators, _) {
+
     var filters = nunjucks.require('filters');
     var globals = nunjucks.require('globals');
 
-    // filters.myFilter = function(text) {...
+    // Filter operator shelves that do not belong to the current operator.
+    filters.for_current = function(shelves) {
+        var current = operators.get.current();
+        return _.filter(shelves, function(shelf) {
+
+            return (shelf.carrier == current.carrier &&
+                    shelf.region == current.region);
+        });
+    };
+
+    // Filter operator shelves by publication status.
+    function by_status(val) {
+        return function(shelves) {
+            var current = operators.get.current();
+            return _.filter(shelves, function(shelf) {
+                return shelf.is_published == val;
+            });
+        };
+    }
+    filters.active = by_status(true);
+    filters.inactive = by_status(false);
 
     // Functions provided in the default context.
     var helpers = {
