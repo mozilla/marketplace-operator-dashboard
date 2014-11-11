882d07e9e6009706b00ca05e4053c47ad6b8d43f
Kevin Ngo <ngoke@mozilla.com>
2014-11-11 07:36:34 -0800

    use hosted settings

--- fabfile.py
+++ fabfile.py
@@ -25,7 +25,7 @@ def update():
     with lcd(PROJECT_NAME):
         local('npm install')
         local('make update')
-        local('cp src/media/js/settings_local.js.dist src/media/js/settings_local.js')
+        local('cp src/media/js/settings_local_hosted.js src/media/js/settings_local.js')
         local('make build')
         local('node_modules/.bin/commonplace langpacks')
 
