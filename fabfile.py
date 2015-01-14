207e888dc7e4f34b1a848bb95671887d9ab61386
Jeremiah Orem <oremj@oremj.com>
2015-01-14 11:24:29 -0800

    fabfile: add jenkins deploy targets

--- fabfile.py
+++ fabfile.py
@@ -38,3 +38,27 @@ def deploy():
                    cluster=settings.CLUSTER,
                    domain=settings.DOMAIN,
                    root=ROOT)
+
+
+@task
+def build():
+    with lcd(PROJECT_NAME):
+        local('npm install')
+        local('make install')
+        local('cp src/media/js/settings_local_hosted.js '
+              'src/media/js/settings_local.js')
+        local('make build')
+        local('node_modules/.bin/commonplace langpacks')
+
+
+@task
+def deploy_jenkins():
+    r = helpers.build_rpm(name=settings.PROJECT_NAME,
+                          app_dir='marketplace-operator-dashboard',
+                          env=settings.ENV,
+                          cluster=settings.CLUSTER,
+                          domain=settings.DOMAIN,
+                          root=ROOT)
+
+    r.local_install()
+    r.remote_install(['web'])
