9224eefe3446a2684a8b138866ca8d162187c057
Kevin Ngo <ngoke@mozilla.com>
2015-03-18 16:30:56 -0700

    mcm v2.4 bump (bug 985552/1144437)

--- src/media/js/init.js
+++ src/media/js/init.js
@@ -1,6 +1,6 @@
 define('init',
-    ['core/init', 'helpers_local', 'routes', 'settings_app'],
-    function(init, helpersLocal, routes, settingsApp) {
-    // Put any code that needs to run to initialize the app here or in the
-    // dependencies.
+    ['core/init', 'helpers_local', 'routes', 'settings_app', 'templates'],
+    function(init, helpersLocal, routes, settingsApp, templates) {
+
+    return init.ready;
 });
